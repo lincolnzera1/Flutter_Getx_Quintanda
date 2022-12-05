c0c0248567d96a929b8e024aa96c22a58e040be5
lincolnzera1 <guilherme.lincoln.lima.11@gmail.com>
2022-12-05 10:03:41 -0300

    getx.exe

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/splash/splash_screen.dart';
+import 'package:greengrocer/src/pages_routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Greengrocer',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white.withAlpha(190),
+      ),
+      debugShowCheckedModeBanner: false,
+      getPages: AppPages.pages,
+      initialRoute: PagesRoutes.splashRoute,
+    );
+  }
+}
---
+++ lib/src/pages_routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
+import 'package:greengrocer/src/pages/auth/sign_up_screen.dart';
+import 'package:greengrocer/src/pages/splash/splash_screen.dart';
+
+abstract class AppPages {
+  static final pages = <GetPage>[
+    GetPage(
+      name: PagesRoutes.splashRoute,
+      page: () => const SplashScreen(),
+    ),
+    GetPage(
+      name: PagesRoutes.signInRoute,
+      page: () => const SignInScreen(),
+    ),
+    GetPage(
+      name: PagesRoutes.signUpRoute,
+      page: () => SignUpScreen(),
+    ),
+  ];
+}
+
+abstract class PagesRoutes {
+  static const String signInRoute = "/signin";
+  static const String signUpRoute = "/signup";
+  static const String splashRoute = "/splash";
+}
